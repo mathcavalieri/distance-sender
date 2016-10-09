00058de4fa2024d49802b5eb23ccaa6434788335
Matheus Cavalieri <math.cavalieri@gmail.com>
2016-10-09 02:04:47 -0300

    Send notification when the gateway ip changes

--- server/config/database.js
+++ server/config/database.js
@@ -10,4 +10,11 @@ var clientSchema = mongoose.Schema({
     token: String
 });
 
-module.exports = mongoose.model('Client', clientSchema);
+var ipSchema = mongoose.Schema({
+    ip: String
+});
+
+module.exports = {
+    client: mongoose.model('Client', clientSchema),
+    ip: mongoose.model('Ip', ipSchema)
+};
